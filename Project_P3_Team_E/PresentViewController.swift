231347254b15c284804379ec07d9dc2c81830611
Malay Bhakta <malaybhakta10@gmail.com>
2019-11-24 15:30:42 -0600

    Audio Player

--- Project_P3_Team_E/PresentViewController.swift
+++ Project_P3_Team_E/PresentViewController.swift
@@ -25,6 +25,9 @@ class PresentViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer?
     var isPlaying: Bool = true
+    var timer: Timer?
+    //for fastforward and rewind
+    var tempTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,14 +70,16 @@ class PresentViewController: UIViewController {
     
     //MARK: - Recording
     @IBAction func volumeSliderAdj(_ sender: Any) {
+        print(volumeSlider.value)
         if audioPlayer != nil{
-            audioPlayer?.volume = volumeSlider.value
+            audioPlayer?.setVolume(volumeSlider.value, fadeDuration: 0.1)
+            //audioPlayer?.volume = volumeSlider.value
         }
     }
     func loadRecordingUI(){
         print("Button here")
         //view.addSubview(recordButton)
-        rButton.title = "Tap To Record1"
+        rButton.title = "Tap To Record"
         
     }
     //Record Button Tapped
@@ -112,7 +117,7 @@ class PresentViewController: UIViewController {
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey : 12000,
             AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue
         ]
         
         do{
@@ -160,44 +165,101 @@ class PresentViewController: UIViewController {
     
     @IBAction func playButtonTapped(_ sender: Any) {
         var items = toolbar.items
-        if (isPlaying){
+        //pause
+        if (!isPlaying){
             print("Change to Pause")
-            items![4] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.pause, target: self, action: #selector(self.playButtonTapped(_:)))
-            playFun()
-            isPlaying = false
-        }
-        else{
-            print("Change to play")
             items![4] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.play, target: self, action: #selector(self.playButtonTapped(_:)))
             isPlaying = true
+            audioPlayer?.stop()
+        }
+        //play
+        else{
+            if audioPlayer == nil{
+                playFun()
+            }
+            audioPlayer?.play()
+            print("Change to play")
+            items![4] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.pause, target: self, action: #selector(self.playButtonTapped(_:)))
+            
+            isPlaying = false
         }
         self.toolbar.setItems(items, animated: true)
         
         
     }
-
+    
+    
+    @IBAction func ffButtonTapped(_ sender: Any) {
+//        tempTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.jumpForward), userInfo: nil, repeats: true)
+        if audioPlayer != nil{
+            var timeInterval: TimeInterval = audioPlayer!.currentTime
+            timeInterval += 5
+            if timeInterval > audioPlayer!.duration{
+                audioPlayer!.stop()
+                print("Stop")
+            }
+            else{
+                
+                print(audioPlayer!.currentTime)
+                audioPlayer!.currentTime = timeInterval
+                if !audioPlayer!.isPlaying{
+                    audioPlayer?.play()
+                }
+            }
+        }
+        else{
+            print("No audio Player")
+        }
+    }
+    
     func playFun(){
         let playFile = getDocumentsDirectory().appendingPathComponent(fName).appendingPathExtension("m4a")
         do{
             audioPlayer = try AVAudioPlayer(contentsOf: playFile)
             audioPlayer?.play()
-            volumeSliderAdj(volumeSlider.value)
-            
+            //volumeSliderAdj(volumeSlider.value)
+            print(playFile.relativePath)
         }catch let error as NSError{
             print("Fail to play \(error)")
         }
     }
+    
+    @IBAction func rewindButtonTapped(_ sender: Any) {
+        
+        if audioPlayer != nil{
+            var timeInterval: TimeInterval = audioPlayer!.currentTime
+            timeInterval -= 5
+            if timeInterval < 0{
+                audioPlayer!.stop()
+                print("Stop")
+                //need to change button to playbutton
+            }
+            else{
+                
+                print(audioPlayer!.currentTime)
+                audioPlayer!.currentTime = timeInterval
+                if !audioPlayer!.isPlaying{
+                    audioPlayer?.play()
+                }
+            }
+        }
+        else{
+            print("No audio Player")
+        }
     }
+    
+}
+
 //MARK: - Extension
 
 extension UIView {
-   /// Flip view horizontally.
-   func flipX() {
-       transform = CGAffineTransform(scaleX: -transform.a, y: transform.d)
-   }
-
-   /// Flip view vertically.
-   func flipY() {
-       transform = CGAffineTransform(scaleX: transform.a, y: -transform.d)
-   }
+    /// Flip view horizontally.
+    func flipX() {
+        transform = CGAffineTransform(scaleX: -transform.a, y: transform.d)
+    }
+    
+    /// Flip view vertically.
+    func flipY() {
+        transform = CGAffineTransform(scaleX: transform.a, y: -transform.d)
+    }
 }
