93b0627c8818e4a5d047dbb6336f03a5ec641aee
Cameron Gaither <cameron.gaither@okstate.edu>
2019-11-24 20:03:23 -0600

    Got the toolbar mostly working.

--- Project_P3_Team_E/SelectedViewController.swift
+++ Project_P3_Team_E/SelectedViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 import MobileCoreServices
+import RealmSwift
+import WebKit
+import PDFKit
 
 class SelectedViewController: UIViewController, UITabBarDelegate, UIDocumentPickerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tabBar: UITabBar!
     @IBOutlet weak var promptName: UITextField!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var edit: UITabBarItem!
+    @IBOutlet weak var export: UITabBarItem!
+    @IBOutlet weak var settings: UITabBarItem!
+    @IBOutlet weak var delete: UITabBarItem!
+    
     
     var prompt: Prompt?
     
@@ -26,6 +34,26 @@ class SelectedViewController: UIViewController, UITabBarDelegate, UIDocumentPick
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         switch item.tag {
+        case 0: //Edit
+            //Save old text in case the user doesn't want the changes
+            
+            //Check if text is already being edited
+            if textView.isEditable == false {
+                textView.isEditable = true
+            }
+            else {
+                textView.isEditable = false
+                let alert = UIAlertController(title: "Would you like to save your changes?", message: "", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+                    self.savePrompt()
+                }))
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+                        let realm = try! Realm()
+                    let oldPrompt = realm.objects(Prompt.self).filter("name = '\(self.promptName.text ?? "")'").first
+                    self.textView.text = oldPrompt?.text
+                }))
+                self.present(alert, animated: true)
+            }
         case 1: //Export
             //get file Name
             let fileNameToSave: String = promptName.text!
@@ -54,11 +82,46 @@ class SelectedViewController: UIViewController, UITabBarDelegate, UIDocumentPick
                     navigator.pushViewController(viewController, animated: true)
                 }
             }
+        case 3: //Delete
+            let alert = UIAlertController(title: "Are you sure you want to delete your prompt?", message: "", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+                let realm = try! Realm()
+                let prompt = realm.objects(Prompt.self).filter("name = '\(self.promptName.text ?? "")'").first
+                try! realm.write {
+                    realm.delete(prompt!)
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
         default:
             print("Default")
         }
     }
 
+    func savePrompt() {
+        if promptName.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Please enter a name", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        if textView.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Please enter text", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let promptDate = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short)
+        
+        let prompt = Prompt(name: promptName.text!, date: promptDate, text: textView.text!)
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(prompt)
+            print("Added prompt \(prompt.name) to Realm.")
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
        if segue.identifier == "presentPrompt" {
            let nextVC = segue.destination as? PresentViewController
